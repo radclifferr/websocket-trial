adb4fcaf222cd5ac795151bff40ab859615a5f66
Richard Radcliffe <107942724+radclifferr@users.noreply.github.com>
2023-12-12 09:39:41 -0800

    Initial Commit

---
+++ Server/index.js
@@ -0,0 +1,12 @@
+import { Server } from "socket.io";
+
+const io = new Server(3000);
+
+io.on("connection", (socket) => {
+
+  socket.emit("hello", "world");
+
+  socket.on("hi", (arg) => {
+    console.log(arg)
+  })
+})
