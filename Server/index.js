9abe3694b29ce0fb7f2701b3f757bc14b5d61080
Richard Radcliffe <radcliffer94@gmail.com>
2023-12-12 10:31:42 -0800

    Minimally reproducable example

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,23 @@
+import { createServer } from "http";
 import { Server } from "socket.io";
 
-const io = new Server(3000);
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials:true
+  }
+});
 
 io.on("connection", (socket) => {
 
   socket.emit("hello", "world");
 
-  socket.on("hi", (arg) => {
+  socket.on("howdy", (arg) => {
     console.log(arg)
   })
 })
+
+httpServer.listen(3000, () => console.log("listening on 3000"));
